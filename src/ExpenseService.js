1a228dcf3c6f82b9ef016c9d57bc465a141ad1a6
Ath1678 <atharvabhadale1678@gmail.com>
2025-11-11 21:09:21 +0530

    fix ExpenseService

--- src/ExpenseService.js
+++ src/ExpenseService.js
@@ -3,7 +3,14 @@ const BASE_URL = "https://expense-tracker-backend-kqge.onrender.com";
 export async function getExpenses() {
   try {
     const res = await fetch(`${BASE_URL}/api/expenses`);
-    return await res.json();
+    const data = await res.json();
+
+    if (!Array.isArray(data)) {
+      console.error("BAD API FORMAT:", data);
+      return [];
+    }
+
+    return data;
   } catch (err) {
     console.error("API Error:", err);
     return [];
@@ -12,13 +19,15 @@ export async function getExpenses() {
 
 export async function addExpense(expense) {
   try {
-    await fetch(`${BASE_URL}/api/expenses`, {
+    const res = await fetch(`${BASE_URL}/api/expenses`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(expense),
     });
+
+    console.log("SAVE RESULT", await res.json());
   } catch (err) {
     console.error("Add Error:", err);
   }
